2c885fe20a57b59ccdfe07065129e879f59a70a9
Ghulam Mustafa Razaq Qadri <mustafa@logico.pk>
2018-10-10 17:17:46 +0500

    Updated

--- hyperladger-fabric/courseBase.sh
+++ hyperladger-fabric/courseBase.sh
@@ -26,12 +26,8 @@ source .bashrc
 go version
 #################################
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
 #################################
-sudo apt-get install python -y
-python --version
-#################################
-sudo apt-get install git
+sudo apt-get install -y nodejs python git
 #################################
 cd ~
 mkdir hyperledger-binaries
